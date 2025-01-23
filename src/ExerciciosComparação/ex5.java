cfb5ec6d1c4abc0dd62f7348bcf3525f1bce97cf
HoFerreira <heigthx@gmail.com>
2025-01-23 10:23:06 -0300

    Mudando arquivos de lugar e adicionando novos exercicios

--- src/ExerciciosComparação/ex5.java
+++ src/ExerciciosComparação/ex5.java
@@ -10,8 +10,26 @@ o A mensagem "Aprovado com Distinção", se a média for igual a
 dez.
 */
 
+import java.util.Scanner;
+
 public class ex5 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        System.out.println("Digite a primeira nota");
+        int nota1 =  scan.nextInt();
+
+        System.out.println("Digite a segunda nota");
+        int nota2 = scan.nextInt();
+
+        double media = (nota1 + nota2) / 2;
+
+        if (media == 10) {
+            System.out.println("Aluno aprovado com distinção com média " + media);
+        } else if (media >= 7){
+            System.out.println("Aluno aprovado com média " + media);
+        } else {
+            System.out.println("Aluno reprovado com média "  + media);
+        }
     }
 }
