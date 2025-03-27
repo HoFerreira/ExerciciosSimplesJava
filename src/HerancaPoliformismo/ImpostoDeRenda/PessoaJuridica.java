dad5797ce6150607cd5bc015bf7ef18048ee19d3
HoFerreira <heigthx@gmail.com>
2025-03-27 10:12:02 -0300

    Herança e poliformismo ex2

---
+++ src/HerancaPoliformismo/ImpostoDeRenda/PessoaJuridica.java
@@ -0,0 +1,30 @@
+package HerancaPoliformismo.ImpostoDeRenda;
+
+public class PessoaJuridica extends Contribuinte{
+
+    private String cnpj;
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    @Override
+    public double calcularImposto() {
+        return this.getRendaBruta() * 0.1;
+        //return (this.getRendaBruta()/100) * 10  poderia ser assim também
+    }
+
+    @Override
+    public String toString() {
+        String s = "Pessoa Jurídica[";
+        s += super.toString();
+        s += " ;cnpj: " + cnpj;
+        s += " ;imposto a ser pago: " + calcularImposto();
+        s += "]";
+        return s;
+    }
+}
